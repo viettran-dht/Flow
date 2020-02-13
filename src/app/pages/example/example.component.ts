b04b3ebde6f0912ad29a329fdba15ac48bb9d5e7
viettran <viet.tranhoang@powergatesoftware.com>
2020-02-13 09:55:07 +0700

    slides

--- src/app/pages/example/example.component.ts
+++ src/app/pages/example/example.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare const Swiper: any;
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
   styleUrls: ['./example.component.scss']
 })
-export class ExampleComponent implements OnInit {
+export class ExampleComponent implements AfterViewInit {
+  name = 'Angular with Swiper';
+  mySwiper: any;
+  slides = [
+    'https://via.placeholder.com/300x200/FF5733/ffffff',
+    'https://via.placeholder.com/300x200/C70039/ffffff',
+    'https://via.placeholder.com/300x200/900C3F/ffffff'
+  ];
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.mySwiper = new Swiper('.swiper-container', {
+      pagination: '.swiper-pagination',
+      paginationClickable: true,
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      autoplay: 3000,
+      spaceBetween: 30
+    });
   }
-
 }
