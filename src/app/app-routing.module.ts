42065ddcb506c1ceae3ebc1c3eb5d221933f7324
ngochiep30051998 <ngochiep30051998@Gmail.com>
2020-02-13 09:39:07 +0700

    init header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+    loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule)
   },
   {
     path: 'login',
@@ -20,45 +20,10 @@ const routes: Routes = [
     loadChildren: () => import('./pages/account-settings/account-settings.module').then(m => m.AccountSettingsModule)
   },
   {
-    path: 'add-new-flow',
-    loadChildren: () => import('./pages/add-new-flow/add-new-flow.module').then(m => m.AddNewFlowModule)
-  },
-  {
-    path: 'billing',
-    loadChildren: () => import('./pages/billing/billing.module').then(m => m.BillingModule)
-  },
-  {
-    path: 'broadcast',
-    loadChildren: () => import('./pages/broadcast/broadcast.module').then(m => m.BroadcastModule)
-  },
-  {
-    path: 'insight',
-    loadChildren: () => import('./pages/insight/insight.module').then(m => m.InsightModule)
-  },
-  {
-    path: 'my-flows',
-    loadChildren: () => import('./pages/my-flows/my-flows.module').then(m => m.MyFlowsModule)
-  },
-  {
-    path: 'tutorial',
-    loadChildren: () => import('./pages/tutorial/tutorial.module').then(m => m.TutorialModule)
-  },
-  {
-    path: 'user-groups',
-    loadChildren: () => import('./pages/user-groups/user-groups.module').then(m => m.UserGroupsModule)
-  },
-  {
     path: 'forgot-password',
     loadChildren: () => import('./pages/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule)
   },
-  {
-    path: 'feature',
-    loadChildren: () => import('./pages/feature/feature.module').then(m => m.FeatureModule)
-  },
-  {
-    path: 'example',
-    loadChildren: () => import('./pages/example/example.module').then(m => m.ExampleModule)
-  },
+  // { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
