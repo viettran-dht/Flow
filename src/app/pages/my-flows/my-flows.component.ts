b2199b63a3c533ec1df2ebcb6b62892744c4bc3d
nguyendanghung <danghungctk50.uct@gmail.com>
2020-02-26 18:34:15 +0700

    Css for my flows page

--- src/app/pages/my-flows/my-flows.component.ts
+++ src/app/pages/my-flows/my-flows.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-flows.component.scss']
 })
 export class MyFlowsComponent implements OnInit {
-
+  public listClient = [
+    { Id: 1, Name: 'hiep' },
+    { Id: 2, Name: 'nam' },
+    { Id: 3, Name: 'dung' }
+  ];
+  public showSelectBox: any = {
+    client: false,
+    brand: false,
+    campaign: false
+  };
   constructor() { }
 
   ngOnInit() {
   }
-
+  onClick(event: any) {
+    // if (!this.zoneSelect.nativeElement.contains(event.target)) {
+    // this.showSelectBox = false;
+    // console.log(event);
+    if (!event.target.id) {
+      for (const key in this.showSelectBox) {
+        if (this.showSelectBox.hasOwnProperty(key)) {
+          this.showSelectBox[key] = false;
+        }
+      }
+    } else {
+      for (const key in this.showSelectBox) {
+        if (this.showSelectBox.hasOwnProperty(key) && key !== event.target.id) {
+          this.showSelectBox[key] = false;
+        }
+      }
+    }
+    // }
+  }
 }
--- src/app/pages/my-flows/my-flows.module.ts
+++ src/app/pages/my-flows/my-flows.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyFlowsComponent } from './my-flows.component';
 import { RouterModule } from '@angular/router';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 
 
@@ -9,6 +10,7 @@ import { RouterModule } from '@angular/router';
   declarations: [MyFlowsComponent],
   imports: [
     CommonModule,
+    ComponentsModule,
     RouterModule.forChild([{path: '', component: MyFlowsComponent}])
   ]
 })
