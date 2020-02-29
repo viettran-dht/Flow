b0828c223e9128d26b616239923bc35918e2e52d
ngochiep30051998 <ngochiep30051998@Gmail.com>
2020-03-01 00:04:04 +0700

    broadcast page

--- src/app/pages/broadcast/broadcast.component.ts
+++ src/app/pages/broadcast/broadcast.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import { EventInput } from '@fullcalendar/core';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-broadcast',
@@ -8,7 +13,14 @@ import { Location } from '@angular/common';
   styleUrls: ['./broadcast.component.scss']
 })
 export class BroadcastComponent implements OnInit {
-
+  @ViewChild('calendar', { static: false }) calendarComponent: FullCalendarComponent; // the #calendar in the template
+  public addAschedule = false;
+  calendarVisible = true;
+  calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
+  calendarWeekends = true;
+  calendarEvents: EventInput[] = [
+    { title: 'Event Now', start: new Date() }
+  ];
   constructor(
     private router: Router,
     private location: Location
@@ -22,5 +34,30 @@ export class BroadcastComponent implements OnInit {
   }
   goback() {
     this.location.back();
+  }
+  toggleVisible() {
+    this.calendarVisible = !this.calendarVisible;
+  }
+
+  toggleWeekends() {
+    this.calendarWeekends = !this.calendarWeekends;
+  }
+
+  gotoPast() {
+    const calendarApi = this.calendarComponent.getApi();
+    calendarApi.gotoDate('2000-01-01'); // call a method on the Calendar object
+  }
+
+  handleDateClick(arg) {
+    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+      this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
+        title: 'New Event',
+        start: arg.date,
+        allDay: arg.allDay
+      });
+    }
+  }
+  handleEventClick(event) {
+    console.log(event);
   }
 }
--- src/app/pages/broadcast/broadcast.module.ts
+++ src/app/pages/broadcast/broadcast.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BroadcastComponent } from './broadcast.component';
 import { RouterModule } from '@angular/router';
-
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 
 @NgModule({
   declarations: [BroadcastComponent],
   imports: [
     CommonModule,
+    FullCalendarModule,
     RouterModule.forChild([{ path: '', component: BroadcastComponent }])
   ]
 })
